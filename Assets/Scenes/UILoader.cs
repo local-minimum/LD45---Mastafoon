0d9a9fc2a347c7e18dad15d8106e1f15d96e28e4
Martin Zackrisson <martin.zackrisson@fripost.org>
2019-10-06 13:52:34 +0200

    A UI loader helper

---
+++ Assets/Scenes/UILoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UILoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        const string uiSceneName = "GameUI";
+        Scene ui = SceneManager.GetSceneByName(uiSceneName);
+        
+        if (!ui.isLoaded) SceneManager.LoadScene(uiSceneName);
+    }
+}
